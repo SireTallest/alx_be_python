d85af8a2e4e6faf5a19a206f9b68b69d4128ea35
Abdulbaki <abdoladepo@gmail.com>
2025-09-09 08:31:13 +0100

    performing basic arithmetic on python

--- basic_operations.py
+++ basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 sum = number1 + number2
 difference = number1 - number2
 product = number1 * number2
-print("The Sum of", number1, "and", number2, "is", sum)
-print("The Difference of", number1, "and", number2, "is", difference)
-print("The Product of", number1, "and", number2, "is", product)
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)

90e5b240ef73f6202dfd7a43640dd236eec65ada
Abdulbaki <abdoladepo@gmail.com>
2025-09-09 08:25:59 +0100

    Creating a basic arithmetic operation python file

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print("The Sum of", number1, "and", number2, "is", sum)
+print("The Difference of", number1, "and", number2, "is", difference)
+print("The Product of", number1, "and", number2, "is", product)

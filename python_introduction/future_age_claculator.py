2f99a88fa238afbf1060c462640b025a84b18887
Abdulbaki <abdoladepo@gmail.com>
2025-09-09 09:09:40 +0100

    performing basic arithmetics using python

---
+++ python_introduction/future_age_claculator.py
@@ -0,0 +1,3 @@
+age = int(input("How old are you? "))
+future_year = 27 + age
+print("In 2050, you will be", future_year, "years old.")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000  # Principal amount in dollars
+rate = 0.05         # Annual interest rate in decimal
+time = 3   #Time in years
+interest = principal * rate * time
+print("The simple interest is:", interest)
